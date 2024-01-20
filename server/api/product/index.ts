9266332e31c948527f8e669cce688c705f476a2c
Ridho Purnomo <72849578+ridopurnomo86@users.noreply.github.com>
2024-01-20 10:48:35 +0700

    feat:add header with no cache

--- server/api/product/index.ts
+++ server/api/product/index.ts
@@ -1,8 +1,19 @@
 import { redis } from '~~/server/lib/redis';
 import prisma from '~~/server/lib/prisma';
 
-export default eventHandler(async () => {
+export default eventHandler(async (event) => {
+  const { headers } = event.req;
   const cachedValue = await redis.get('products');
+
+  if (headers['Cache-Type'] === 'no-cache') {
+    const data = await prisma.product.findMany({
+      include: {
+        Type_Product: true,
+        Category_Product: true,
+      },
+    });
+    return data;
+  }
 
   if (!cachedValue) {
     const data = await prisma.product.findMany({
